d8327110b72b6bb6baa89f957e1f574a3e912d91
Jia Yi Lai <anniuu7134@gmail.com>
2017-10-12 19:50:55 +0800

    更改contoroller

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,12 @@ Route::get('/news',['as'=>'news.index',function(){
 }]);
 */
 
-Route::get('/',['as'=>'home.index','uses'=>
-    'HomeController@index']);
+Route::get('/', function () {
+    return view('home');
+});
 
-Route::get('/',['as'=>'about.index','uses'=>
-    'AboutController@index']);
+Route::get('home',['as'=>'home.index','uses'=> 'HomeController@index']);
 
-Route::get('/',['as'=>'news.index','uses'=>
-    'NewsController@index']);
+Route::get('about',['as'=>'about.index','uses'=> 'AboutController@index']);
 
-
+Route::get('news',['as'=>'news.index','uses'=>  'NewsController@index']);
